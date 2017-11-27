79dab128da4b68f4f685d7252f1d0b112a713e2f
Mauricio Mercado <mau@maumercado.com>
2017-11-26 18:30:23 -0800

    Add company relations with users

--- schema/schema.js
+++ schema/schema.js
@@ -3,12 +3,34 @@ const graphql = require("graphql");
 const axios = require("axios");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: "Company",
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
+
 const UserType = new GraphQLObjectType({
     name: "User",
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType,
+            async resolve ({ companyId }) {
+                try {
+                    const response = await axios.get(
+                        `http://localhost:3000/companies/${companyId}`
+                    );
+                    return response.data;
+                } catch (error) {
+                    return error;
+                }
+            }
+        }
     }
 });
 
@@ -20,7 +42,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } },
             async resolve (parentValue, { id }) {
                 try {
-                    let response = await axios.get(
+                    const response = await axios.get(
                         `http://localhost:3000/users/${id}`
                     );
                     return response.data;
